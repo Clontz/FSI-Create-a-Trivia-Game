377ef318add83222c4a6edfb6a1bff17e0faec94
Isaac Cloninger <ijcloninger@gmail.com>
2022-07-04 11:35:01 -0400

    complex questions implemented

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let userName = window.prompt('Please enter your name:')
+let userName = window.prompt('Please enter your name:');
+let userScore = 0;
+let playAgain = true;
 
+function playGame() {
 for(let i = 0; i < questions.length; i++) {
-    console.log(questions[i])
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    console.log(userAnswer);
+    if (userAnswer === question.correctAnswer) {
+        console.log('Correct')
+        userScore = userScore + 10;
+    }
+}
+
+window.alert ('Your score is: ' +userScore)
+
+}
+while (playAgain === true) {
+    playGame()
+    let userChoice = ('Would you like to play the game again? Yes or No')
+    if(userChoice = 'Yes'){
+        playAgain = true;
+    }
+    else {
+        playAgain = false;
+        window.alert('Thanks for playing the game!')
+    }
 }
